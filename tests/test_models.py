9342cd4aab1785121011b4a44add5a553f1926e9
James Hodson <james.hodson@ukaea.uk>
2022-11-04 09:25:35 +0000

    Add test cases for daily_min/max

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
 
 def test_daily_mean_zeros():
@@ -29,3 +30,49 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+def test_daily_max_zeros():
+    from inflammation.models import daily_max
+
+    test_input = np.array([[0, 0],
+                           [0, 0],
+                           [0, 0]])
+    test_result = np.array([0,0])
+
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+def test_daily_max_integers():
+    from inflammation.models import daily_max
+
+    test_input = np.array([[5, 2, 9],
+                           [36, 4, 31],
+                           [5, 62, 7]])
+    test_result = np.array([36,62,31])
+
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+def test_daily_min_zeros():
+    from inflammation.models import daily_min
+
+    test_input = np.array([[0, 0],
+                           [0, 0],
+                           [0, 0]])
+    test_result = np.array([0,0])
+
+    npt.assert_array_equal(daily_min(test_input), test_result)
+
+def test_daily_min_integers():
+    from inflammation.models import daily_min
+
+    test_input = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([1,2])
+
+    npt.assert_array_equal(daily_min(test_input), test_result)
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
